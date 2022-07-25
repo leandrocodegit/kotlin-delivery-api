503d29f89a4b6443f1c61fb79330b1157563a8b7
lopv <leandro.oliveira@gft.com>
2022-07-25 11:28:33 -0300

    Jenkins create

--- src/test/kotlin/com/api/delivery/repository/DeliveryRepositoryTest.kt
+++ src/test/kotlin/com/api/delivery/repository/DeliveryRepositoryTest.kt
@@ -4,6 +4,7 @@ import com.api.delivery.build.Builder
 import com.api.delivery.model.Delivery
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import org.bson.types.ObjectId
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -45,6 +46,6 @@ class DeliveryRepositoryTest {
     @Test
     fun `test get delivery this client and delivery id`(){
         val findDelivery:  Delivery  = deliveryRepository.findByIdAndIdClient(delivery.id, delivery.idClient).get()
-        assert(findDelivery.id == delivery.id)
+        assert(findDelivery.id == ObjectId(""))
     }
 }
